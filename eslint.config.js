ffed44edaf53975d749a71e83663d90a81b7f513
ZZJsky123 <1647372420@qq.com>
2026-02-19 17:33:06 +0800

    Update ESLint configuration to include additional formatters for CSS, HTML, and Markdown. Adjust VSCode settings to refine ESLint validation file types.

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,11 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   ignores: ['*.md'],
-  formatters: true,
+  formatters: {
+    css: true,
+    html: true,
+    markdown: true,
+  },
   markdown: {
     overrides: {},
   },
