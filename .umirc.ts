0cba553ab6ed63e25ed9c5eef0b45e67af5568c0
ZZJsky123 <1647372420@qq.com>
2026-02-19 21:40:18 +0800

    Refactor project to use @umijs/max, updating configurations and dependencies. Enhanced .umirc.ts with routing, locale, and alias settings. Cleaned up package.json and pnpm-lock.yaml for new dependencies. Added base.less and routes/index.tsx files, and modified .gitignore to exclude .mfsu.

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'umi'
+import path from 'node:path'
+import { defineConfig } from '@umijs/max'
 
 export default defineConfig({
   routes: [
@@ -6,4 +7,33 @@ export default defineConfig({
     { path: '/docs', component: 'docs' },
   ],
   npmClient: 'pnpm',
+  antd: {
+    compact: false,
+    configProvider: {},
+  },
+  locale: {
+    default: 'zh-CN',
+    antd: true,
+    title: true,
+    baseNavigator: false,
+    baseSeparator: '-',
+  },
+  alias: {
+    components: path.resolve(__dirname, 'src/components'),
+    pages: path.resolve(__dirname, 'src/pages'),
+    utils: path.resolve(__dirname, 'src/utils'),
+    assets: path.resolve(__dirname, 'src/assets'),
+    styles: path.resolve(__dirname, 'src/styles'),
+    hooks: path.resolve(__dirname, 'src/hooks'),
+    services: path.resolve(__dirname, 'src/services'),
+    types: path.resolve(__dirname, 'src/types'),
+    configs: path.resolve(__dirname, 'src/configs'),
+  },
+  hash: true,
+  lessLoader: {
+    modifyVars: {
+      hack: `true; @import "~@/theme/base.less";`,
+    },
+  },
+  proxy: {},
 })
